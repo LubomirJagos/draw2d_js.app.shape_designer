1b744491998d9d39f89c31e2eeb0b6b94c90ab8e
LubomirJagos <lubomir.jagos@gmail.com>
2019-04-14 17:43:29 +0200

    Added new super basic scheme.

---
+++ GraphLangDefaultSymbols/template_11.js
@@ -0,0 +1,673 @@
+var GraphLangSymbolTemplate = [
+  {
+    "type": "shape_designer.figure.ExtPort",
+    "id": "059d8d88-b4a1-d529-0873-eb3f7aacee84",
+    "x": 550.3732154368004,
+    "y": 376.6149447680002,
+    "width": 10,
+    "height": 10,
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "in1",
+      "type": "Input",
+      "direction": 3,
+      "fanout": 20
+    },
+    "cssClass": "shape_designer_figure_ExtPort",
+    "ports": [],
+    "bgColor": "#37B1DE",
+    "color": "#1B1B1B",
+    "stroke": 1,
+    "dasharray": null,
+    "filters": [
+      {
+        "name": "shape_designer.filter.PositionFilter"
+      },
+      {
+        "name": "shape_designer.filter.FanoutFilter"
+      },
+      {
+        "name": "shape_designer.filter.PortDirectionFilter"
+      },
+      {
+        "name": "shape_designer.filter.PortTypeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtPort",
+    "id": "19f6e161-d289-7361-98c2-fcde6a1d9707",
+    "x": 550.3732154368004,
+    "y": 415.8602084352002,
+    "width": 10,
+    "height": 10,
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "errorIn",
+      "type": "Input",
+      "direction": 3,
+      "fanout": 20
+    },
+    "cssClass": "shape_designer_figure_ExtPort",
+    "ports": [],
+    "bgColor": "#37B1DE",
+    "color": "#1B1B1B",
+    "stroke": 1,
+    "dasharray": null,
+    "filters": [
+      {
+        "name": "shape_designer.filter.PositionFilter"
+      },
+      {
+        "name": "shape_designer.filter.FanoutFilter"
+      },
+      {
+        "name": "shape_designer.filter.PortDirectionFilter"
+      },
+      {
+        "name": "shape_designer.filter.PortTypeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtPort",
+    "id": "367a4074-320b-5bd5-9d47-b6737c26d9f1",
+    "x": 661.6531337216004,
+    "y": 415.8602084352002,
+    "width": 10,
+    "height": 10,
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "errorOut",
+      "type": "Output",
+      "direction": 1,
+      "fanout": 20
+    },
+    "cssClass": "shape_designer_figure_ExtPort",
+    "ports": [],
+    "bgColor": "#37B1DE",
+    "color": "#1B1B1B",
+    "stroke": 1,
+    "dasharray": null,
+    "filters": [
+      {
+        "name": "shape_designer.filter.PositionFilter"
+      },
+      {
+        "name": "shape_designer.filter.FanoutFilter"
+      },
+      {
+        "name": "shape_designer.filter.PortDirectionFilter"
+      },
+      {
+        "name": "shape_designer.filter.PortTypeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.PolyRect",
+    "id": "f1a835e8-47b8-2c32-4c19-cfc57e0f2248",
+    "x": 571.5661946880003,
+    "y": 368.2598912000002,
+    "width": 78,
+    "height": 68,
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Rectangle"
+    },
+    "cssClass": "shape_designer_figure_PolyRect",
+    "ports": [],
+    "bgColor": "#FFFFFF",
+    "color": "#303030",
+    "stroke": 1,
+    "radius": 0,
+    "dasharray": null,
+    "vertices": [
+      {
+        "x": 571.5661946880003,
+        "y": 368.2598912000002
+      },
+      {
+        "x": 649.5661946880003,
+        "y": 368.2598912000002
+      },
+      {
+        "x": 649.5661946880003,
+        "y": 436.2598912000002
+      },
+      {
+        "x": 571.5661946880003,
+        "y": 436.2598912000002
+      }
+    ],
+    "blur": 0,
+    "filters": [
+      {
+        "name": "shape_designer.filter.PositionFilter"
+      },
+      {
+        "name": "shape_designer.filter.SizeFilter"
+      },
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      },
+      {
+        "name": "shape_designer.filter.FillColorFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLabel",
+    "id": "273f6cd0-fecf-462e-a00c-97e4ed92c213",
+    "x": 561.3732154368004,
+    "y": 339.2911412000002,
+    "width": 28.515625,
+    "height": 21.3125,
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Label"
+    },
+    "cssClass": "shape_designer_figure_ExtLabel",
+    "ports": [],
+    "bgColor": "none",
+    "color": "#1B1B1B",
+    "stroke": 0,
+    "radius": 0,
+    "dasharray": null,
+    "text": "analogWrite",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "fontSize": 16,
+    "fontColor": "#080808",
+    "fontFamily": null,
+    "editor": "draw2d.ui.LabelInplaceEditor",
+    "filters": [
+      {
+        "name": "shape_designer.filter.PositionFilter"
+      },
+      {
+        "name": "shape_designer.filter.FontSizeFilter"
+      },
+      {
+        "name": "shape_designer.filter.FontColorFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "32fd10d5-87e1-d9e3-091e-fecfaba69ced",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 555.2587407360004,
+        "y": 421.4772203519999
+      },
+      {
+        "x": 571.0964326400009,
+        "y": 421.2087848959999
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "9266d7b0-429d-d8b5-3ede-4db2f4938252",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 555.5942850560004,
+        "y": 381.3125652480003
+      },
+      {
+        "x": 571.1970959359999,
+        "y": 381.6816640000002
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "d5ae0d80-6222-9a95-bd80-999a3a314e63",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 650.2848921600006,
+        "y": 420.94034944000066
+      },
+      {
+        "x": 656.7441203200005,
+        "y": 420.94034944000066
+      },
+      {
+        "x": 663.2033484800006,
+        "y": 420.94034944000066
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "b34f858b-0446-be73-6aa9-99ea6dd1fe1e",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 583.0095981772802,
+        "y": 397.75692128256014
+      },
+      {
+        "x": 583.5894187622403,
+        "y": 429.93696374784014
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "159c4ecf-84e8-cea8-1350-c53db4a42ce9",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 583.5894187622403,
+        "y": 419.5001932185602
+      },
+      {
+        "x": 616.6391921049602,
+        "y": 419.21028292608014
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "b0b6a901-3767-0158-7328-183e8b5c184e",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 583.8793290547202,
+        "y": 419.5001932185602
+      },
+      {
+        "x": 586.1986113945602,
+        "y": 401.52575508480015
+      },
+      {
+        "x": 586.7784319795202,
+        "y": 429.35714316288016
+      },
+      {
+        "x": 589.0977143193602,
+        "y": 408.19369181184015
+      },
+      {
+        "x": 591.9968172441602,
+        "y": 425.29839906816017
+      },
+      {
+        "x": 592.8665481216002,
+        "y": 402.68539625472016
+      },
+      {
+        "x": 596.0555613388802,
+        "y": 430.2268740403202
+      },
+      {
+        "x": 598.3748436787203,
+        "y": 410.2230638592002
+      },
+      {
+        "x": 602.4335877734402,
+        "y": 425.29839906816017
+      },
+      {
+        "x": 604.1730495283202,
+        "y": 411.38270502912013
+      },
+      {
+        "x": 607.6519730380802,
+        "y": 425.29839906816017
+      },
+      {
+        "x": 611.1308965478403,
+        "y": 399.20647274496014
+      },
+      {
+        "x": 612.8703583027202,
+        "y": 425.29839906816017
+      },
+      {
+        "x": 615.1896406425602,
+        "y": 420.08001380352016
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "32f7a1e4-e1c3-b5f8-addd-601a77f2e925",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 627.9456935116802,
+        "y": 379.78248314880017
+      },
+      {
+        "x": 629.6851552665603,
+        "y": 377.46320080896015
+      },
+      {
+        "x": 632.2943478988802,
+        "y": 377.75311110144014
+      },
+      {
+        "x": 634.6136302387202,
+        "y": 379.78248314880017
+      },
+      {
+        "x": 634.0338096537603,
+        "y": 382.97149636608015
+      },
+      {
+        "x": 632.0044376064002,
+        "y": 384.42104782848014
+      },
+      {
+        "x": 620.9878464921602,
+        "y": 396.59728011264014
+      },
+      {
+        "x": 615.1896406425602,
+        "y": 396.30736982016015
+      },
+      {
+        "x": 615.4795509350403,
+        "y": 391.08898455552014
+      },
+      {
+        "x": 627.6557832192002,
+        "y": 379.4925728563201
+      },
+      {
+        "x": 632.0044376064002,
+        "y": 384.71095812096013
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "9d6ac663-ba35-3e32-a18b-702762f10521",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 615.7694612275202,
+        "y": 391.37889484800013
+      },
+      {
+        "x": 620.6979361996803,
+        "y": 396.59728011264014
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "d066aa3f-ac97-1447-afea-dbaa085d6738",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 616.6391921049602,
+        "y": 392.8284463104001
+      },
+      {
+        "x": 628.2356038041603,
+        "y": 380.65221402624013
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "4a968b37-1547-bf89-90d8-015b178ebfb8",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 618.9584744448002,
+        "y": 393.98808748032013
+      },
+      {
+        "x": 629.6851552665603,
+        "y": 382.68158607360016
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "59daabe5-6c1c-0455-ea47-870658634c80",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 619.2483847372803,
+        "y": 394.85781835776015
+      },
+      {
+        "x": 631.1347067289603,
+        "y": 383.5513169510401
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  },
+  {
+    "type": "shape_designer.figure.ExtLine",
+    "id": "ef949e49-013e-bb92-f314-174ede2af983",
+    "alpha": 1,
+    "angle": 0,
+    "userData": {
+      "name": "Line"
+    },
+    "cssClass": "shape_designer_figure_ExtLine",
+    "stroke": 1,
+    "color": "#000000",
+    "outlineStroke": 0,
+    "outlineColor": "none",
+    "policy": "draw2d.policy.line.VertexSelectionFeedbackPolicy",
+    "vertex": [
+      {
+        "x": 629.1053346816002,
+        "y": 378.0430213939201
+      },
+      {
+        "x": 633.4539890688002,
+        "y": 382.1017654886401
+      }
+    ],
+    "router": "draw2d.layout.connection.VertexRouter",
+    "radius": "",
+    "filters": [
+      {
+        "name": "shape_designer.filter.StrokeFilter"
+      }
+    ]
+  }
+];
